737a953589383cb6be8098c6af9c79c1a507ecf8
cyriehe <nabilahfirmansyah7@gmail.com>
2024-03-07 20:35:00 +0700

    soal soal

--- script.js
+++ script.js
@@ -1,56 +1,55 @@
 const options = {
-  aroma: "Pleasing smell",
-  pepper: "Salt's partner",
-  halt: "put a stop to",
-  jump: "Rise suddenly ",
-  shuffle: "Mix cards up ",
-  combine: "Add; Mix",
-  chaos: "Total disorder",
-  labyrinth: "Maze",
-  disturb: "Interrupt; upset ",
-  shift: "Move; Period of word",
-  machine: "Device or appliance",
+  Paleotikum: 'Nama lain dari zaman batu tua adalah',
+  Papua: 'Penyebaran jenis kapak lonjong ini terutama di kepulauan Indonesia bagian timur salah satunya adalah',
+  Waruga: 'Peti jenazah kecil yang berbentuk kubus dan ditutup dengan batu lain yang berbentuk atap rumah',
+  genggam: 'Kapak yang digunakan untuk menusuk binatang atau menggali tanah saat mencari umbi-umbian merupakan ',
+  VonSteinCallenfels: 'Kebudayaan abris sous roche merupakan kebudayaan yang ditemukan di gua-gua, yang diteliti oleh ',
+  Dolmen: 'Hasil Kebudayaan Zaman Megalitikum salah satunya',
+  walzeinbeil: 'Kapak yang ukuran besar sering disebut',
+  Pertanian: 'Kapak persegi ini cocok sebagai alat',
+  Menhir: 'Tiang atau tugu batu yang terbuat dari batu tunggal dan ditempatkan pada suatu tempat merupakan penjelasan dari ',
+  Trapesium: 'Kapak persegi ini berbentuk persegi panjang dan ada juga yang berbentuk',
 };
 //Initial References
-const message = document.getElementById("message");
-const hintRef = document.querySelector(".hint-ref");
-const controls = document.querySelector(".controls-container");
-const startBtn = document.getElementById("start");
-const letterContainer = document.getElementById("letter-container");
-const userInpSection = document.getElementById("user-input-section");
-const resultText = document.getElementById("result");
-const word = document.getElementById("word");
+const message = document.getElementById('message');
+const hintRef = document.querySelector('.hint-ref');
+const controls = document.querySelector('.controls-container');
+const startBtn = document.getElementById('start');
+const letterContainer = document.getElementById('letter-container');
+const userInpSection = document.getElementById('user-input-section');
+const resultText = document.getElementById('result');
+const word = document.getElementById('word');
 const words = Object.keys(options);
-let randomWord = "",
-  randomHint = "";
+let randomWord = '',
+  randomHint = '';
 let winCount = 0,
   lossCount = 0;
 //Generate random value
 const generateRandomValue = (array) => Math.floor(Math.random() * array.length);
 //Block all the buttons
 const blocker = () => {
-  let lettersButtons = document.querySelectorAll(".letters");
+  let lettersButtons = document.querySelectorAll('.letters');
   stopGame();
 };
 //Start Game
-startBtn.addEventListener("click", () => {
-  controls.classList.add("hide");
+startBtn.addEventListener('click', () => {
+  controls.classList.add('hide');
   init();
 });
 //Stop Game
 const stopGame = () => {
-  controls.classList.remove("hide");
+  controls.classList.remove('hide');
 };
 //Generate Word Function
 const generateWord = () => {
-  letterContainer.classList.remove("hide");
-  userInpSection.innerText = "";
+  letterContainer.classList.remove('hide');
+  userInpSection.innerText = '';
   randomWord = words[generateRandomValue(words)];
   randomHint = options[randomWord];
   hintRef.innerHTML = `<div id="wordHint">
   <span>Hint: </span>${randomHint}</div>`;
-  let displayItem = "";
-  randomWord.split("").forEach((value) => {
+  let displayItem = '';
+  randomWord.split('').forEach((value) => {
     displayItem += '<span class="inputSpace">_ </span>';
   });
   //Display each element as span
@@ -61,40 +60,40 @@ const generateWord = () => {
 const init = () => {
   winCount = 0;
   lossCount = 5;
-  randomWord = "";
-  word.innerText = "";
-  randomHint = "";
-  message.innerText = "";
-  userInpSection.innerHTML = "";
-  letterContainer.classList.add("hide");
-  letterContainer.innerHTML = "";
+  randomWord = '';
+  word.innerText = '';
+  randomHint = '';
+  message.innerText = '';
+  userInpSection.innerHTML = '';
+  letterContainer.classList.add('hide');
+  letterContainer.innerHTML = '';
   generateWord();
   //For creating letter buttons
   for (let i = 65; i < 91; i++) {
-    let button = document.createElement("button");
-    button.classList.add("letters");
+    let button = document.createElement('button');
+    button.classList.add('letters');
     //Number to ASCII[A-Z]
     button.innerText = String.fromCharCode(i);
     //Character button onclick
-    button.addEventListener("click", () => {
+    button.addEventListener('click', () => {
       message.innerText = `Correct Letter`;
-      message.style.color = "#008000";
-      let charArray = randomWord.toUpperCase().split("");
-      let inputSpace = document.getElementsByClassName("inputSpace");
+      message.style.color = '#008000';
+      let charArray = randomWord.toUpperCase().split('');
+      let inputSpace = document.getElementsByClassName('inputSpace');
       //If array contains clicked value replace the matched Dash with Letter
       if (charArray.includes(button.innerText)) {
         charArray.forEach((char, index) => {
           //If character in array is same as clicked button
           if (char === button.innerText) {
-            button.classList.add("correct");
+            button.classList.add('correct');
             //Replace dash with letter
             inputSpace[index].innerText = char;
             //increment counter
             winCount += 1;
             //If winCount equals word length
             if (winCount == charArray.length) {
-              resultText.innerHTML = "You Won";
-              startBtn.innerText = "Restart";
+              resultText.innerHTML = 'You Won';
+              startBtn.innerText = 'Next';
               //block all buttons
               blocker();
             }
@@ -102,16 +101,14 @@ const init = () => {
         });
       } else {
         //lose count
-        button.classList.add("incorrect");
+        button.classList.add('incorrect');
         lossCount -= 1;
-        document.getElementById(
-          "chanceCount"
-        ).innerText = `Chances Left: ${lossCount}`;
+        document.getElementById('chanceCount').innerText = `Chances Left: ${lossCount}`;
         message.innerText = `Incorrect Letter`;
-        message.style.color = "#ff0000";
+        message.style.color = '#ff0000';
         if (lossCount == 0) {
           word.innerHTML = `The word was: <span>${randomWord}</span>`;
-          resultText.innerHTML = "Game Over";
+          resultText.innerHTML = 'Game Over';
           blocker();
         }
       }
